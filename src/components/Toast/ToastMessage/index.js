006e2cf199fc50664657bbbc66f07ad2d70c0ee6
Kauanphbbb <kauanphbbb@gmail.com>
2024-02-20 17:40:45 -0300

    feat: remove toast after a time

--- src/components/Toast/ToastMessage/index.js
+++ src/components/Toast/ToastMessage/index.js
@@ -1,10 +1,21 @@
 import PropTypes from 'prop-types';
+import { useEffect } from 'react';
 import { Container } from './styles';
 
 import checkCircleIcon from '../../../assets/images/icons/check-circle.svg';
 import xCircleIcon from '../../../assets/images/icons/x-circle.svg';
 
 export default function ToastMessage({ message, onRemoveMessage }) {
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      onRemoveMessage(message.id);
+    }, message.duration || 7000);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [message, onRemoveMessage]);
+
   function handleRemoveToast() {
     onRemoveMessage(message.id);
   }
@@ -28,5 +39,6 @@ ToastMessage.propTypes = {
     id: PropTypes.number.isRequired,
     text: PropTypes.string.isRequired,
     type: PropTypes.oneOf(['default', 'danger', 'success']),
+    duration: PropTypes.number,
   }).isRequired,
 };
--- src/utils/toast.js
+++ src/utils/toast.js
@@ -2,6 +2,6 @@ import EventManager from '../lib/EventManger';
 
 export const toastEventManager = new EventManager();
 
-export default function toast({ type, text }) {
-  toastEventManager.emit('addToast', { type, text });
+export default function toast({ type, text, duration }) {
+  toastEventManager.emit('addToast', { type, text, duration });
 }
