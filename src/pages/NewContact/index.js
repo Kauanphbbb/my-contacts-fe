d7290efc1a33ac4aa9202a621260a14199e386fb
kauanphbbb <kauanphbbb@gmail.com>
2024-02-24 18:24:23 -0300

    feat: clear form when creates a new contact

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,9 +1,11 @@
+import { useRef } from 'react';
 import ContactForm from '../../components/ContactForm';
 import PageHeader from '../../components/PageHeader';
 import ContactsService from '../../services/ContactsService';
 import toast from '../../utils/toast';
 
 export default function NewContact() {
+  const contactFormRef = useRef(null);
   async function handleSubmit(formData) {
     try {
       const contact = {
@@ -13,6 +15,9 @@ export default function NewContact() {
         category_id: formData.categoryId,
       };
       await ContactsService.createContacts(contact);
+
+      contactFormRef.current.resetFields();
+
       toast({
         type: 'success',
         text: 'Contato cadastrado com sucesso!',
@@ -28,7 +33,7 @@ export default function NewContact() {
   return (
     <>
       <PageHeader title="Novo contato" />
-      <ContactForm buttonLabel="Cadastrar" onSubmit={handleSubmit} />
+      <ContactForm buttonLabel="Cadastrar" onSubmit={handleSubmit} ref={contactFormRef} />
     </>
   );
 }
